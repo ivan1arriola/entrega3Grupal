f86f12cfb090402de823f6b9ad505e3605b816d4
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-17 20:39:13 -0300

    se mejoro la presentacion de los datos

--- js/space.js
+++ js/space.js
@@ -1,8 +1,10 @@
 const NASA_API = 'https://images-api.nasa.gov/search?q=';
-const MEDIA_TYPE = '&media_type=image'; // Se agrego un parametro para pedir que solo devuelva imagenes (excluyendo videos de las respuestas)
+const MEDIA_TYPE = '&media_type=image'; 
+// Se agrego un parametro para pedir que solo devuelva imagenes (excluyendo videos y audios de las respuestas)
 let data = [];
 
 const getJSONData = async (input) => {
+    console.log(NASA_API + input + MEDIA_TYPE)
     const result = await fetch(NASA_API + input + MEDIA_TYPE);
     if (result.ok) {
         const response = await result.json();
@@ -14,30 +16,36 @@ const getJSONData = async (input) => {
 
 const showResult = (result) => {
     
-    let html = "";
+    let html = `<div class="card-deck card-group">`;
     result.forEach((item, index) => {
+
+        const imgLink = item.links[0].href.replace(/\s+/g, '%20'); 
+        // uso replace para reemplazar los espacios en blanco del link por %20 para que el navegador lo interprete adecuadamente
+
         html += `
-        <div class="result">
-     
-        <div class="flex">
-        
-        <div class="img-result block">
-          <img src=${item.links[0].href} alt="${index}"  >
-        </div>
-        
-        <div class="block">
-            <h1>${item.data[0].title}</h1> 
-           <div class="des"><p>${item.data[0].description}</p></div>
-        </div>
-        </div> 
-        
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=${imgLink} alt="${index}">
+            <div class="card-body">
+                <h5 class="card-title">${item.data[0].title}</h5>
+                <p class="card-text">${item.data[0].description}</p>
+            </div>
         </div>
         `
+        
+        if(index!= 0 && index % 4 == 3) {
+            html += `
+            </div>
+            <div class="card-deck card-group">
+            `
+        }
     });
+
+    html += `</div>`;
+
+
     document.getElementById('contenedor').innerHTML = html;
 
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const btnBuscar = document.getElementById('btnBuscar')
@@ -47,16 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
         await getJSONData(inputBuscar);
         showResult(data);
 
-
-
-
-
     })
-        ;
-
-
-
-
-
 
 })
